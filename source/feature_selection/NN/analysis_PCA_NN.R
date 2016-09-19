7fbcfbb2deeb1f8e9f572e9fd28ed76f28580aeb
deybanperez <deyban.perez@gmail.com>
2016-09-19 19:18:59 -0400

    Testing GFR

---
+++ source/feature_selection/NN/analysis_PCA_NN.R
@@ -0,0 +1,34 @@
+#Preparing environment
+rm(list = ls())
+setwd("/home/dperez/Documents/Repos/Tesis/source")
+source("functions/functions.R")
+require("nnet")
+
+#Loading the results
+results  = readRDS("feature_selection/NN/results_PCA.rds")
+###########################################################
+#Creating new variables
+sd.results = vector(mode = "numeric", length = nrow(results))
+mean.results = vector(mode = "numeric", length = nrow(results))
+
+#Calculating standard deviation
+for (i in 1:length(sd.results))
+  sd.results[i] = sd(results[i,])
+
+#Calculating mean
+for (i in 1:length(mean.results))
+  mean.results[i] = mean(results[i,])
+
+##########################################################
+#Splitting the screen in 2
+par(mfrow = c(1,2))
+
+#Plotting standard deviation vs number of components
+plot(sd.results, col = "blue", type = "b",
+     main = "Standard Deviation vs Number of Components",
+     xlab = "Number of Components", ylab = "Standard Deviation")
+
+#Plotting mean accuracy vs number of components
+plot(mean.results, col = "blue", type = "b",
+     main = "Mean Accuracy vs Number of Components",
+     xlab = "Number of Components", ylab = "Mean Accuracy")
